ae0c3e4c9236643d5705170f7d9987f6618d6733
Shaurya <agarwal.lucky.shaurya11!gmail.com>
2019-04-20 14:05:27 +0530

    New Commit

--- gitCommit.sh
+++ gitCommit.sh
@@ -6,6 +6,7 @@ isgit=${remote: : -4}
 if [ "$isgit" != ".git" ] ( remote+=".git" && set url=$remote ) else ( url=$remote%.git ) fi
 git remote set-url origin $remote
 git add .
+git rm --cached $0
 if [ "%2"=="" ] ( msg="New Commit" ) else ( msg=$2 ) fi
 git commit -m $msg
 git pull origin master
